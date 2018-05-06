ed17e2df80af092b98b899380eb018c1e5f1f493
Beaglefoot <ch-e-st@yandex.ru>
2018-05-06 13:01:38 +0300

    Update modifier test

--- users/test/update_test.js
+++ users/test/update_test.js
@@ -5,7 +5,7 @@ describe('Updating records', () => {
   let joe;
 
   beforeEach(done => {
-    joe = new User({ name: 'Joe' });
+    joe = new User({ name: 'Joe', postCount: 0 });
     joe.save().then(() => done());
   });
 
@@ -38,5 +38,12 @@ describe('Updating records', () => {
 
   it('A model class can find a records with a given Id and update it', done => {
     assertName(User.findByIdAndUpdate(joe._id, { name: 'Alex' }), done);
+  });
+
+  it('A user can have their post count incremented by 1', done => {
+    User.update({ name: 'Joe' }, { $inc: { postCount: 1 } })
+      .then(() => User.findOne({ name: 'Joe' }))
+      .then(user => assert(user.postCount === 1))
+      .then(done);
   });
 });
